c51ec395dcb21c429d5f56a00284645571767404
Wang Lei <01012592@r.recruit.co.jp>
2017-08-24 17:38:02 +0900

    comment is added

--- cfg/dist.js
+++ cfg/dist.js
@@ -14,17 +14,17 @@ let config = Object.assign({}, baseConfig, {
   cache: false,
   devtool: 'sourcemap',
   plugins: [
-    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.DedupePlugin(),//用来检查相似的文件，或者文件中重复的内容，然后将这些冗余再输出时消除
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     }),
     new BowerWebpackPlugin({
       searchResolveModulesDirectories: false
     }),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.optimize.UglifyJsPlugin(), //压缩输出js代码
+    new webpack.optimize.OccurenceOrderPlugin(), //按照引用频度来排序各个模块，bundle的id，引用越频繁，其id值就越短，以便达到减小代码的效果
+    new webpack.optimize.AggressiveMergingPlugin(), //用来优化生成的代码段，合并代码track，汲取公共部分
+    new webpack.NoErrorsPlugin() //保证编译过程不能出错
   ],
   module: defaultSettings.getDefaultModules()
 });
--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
+        <img src={yeomanImage} alt="Yeoman Generator" /><span>hello</span>
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
